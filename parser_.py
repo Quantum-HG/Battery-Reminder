e909b0dca9f46a484db04a39738fe53345d5efc0
Harsh Gupta <119344859+Quantum-HG@users.noreply.github.com>
2025-05-15 14:44:48 +0530

    Delete parser_.py

--- parser_.py
+++
@@ -1,9 +0,0 @@
-# parser_.py
-from bs4 import BeautifulSoup
-
-def parse_html(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        html = file.read()
-    soup = BeautifulSoup(html, 'html.parser')
-    title = soup.title.string if soup.title else 'No Title Found'
-    return title
